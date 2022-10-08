7a8a512f98eb5076f69c5fc145f6685fa3f8f4b1
Grunt395 <Grantlew@att.net>
2022-10-07 21:18:10 -0700

    Added getOne and post routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -24,10 +24,33 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [Product]
+    });
+
+    if (!categoryData) {
+      res.status(400).json({ message: "There is no category with that id."})
+      return
+    }
+    res.status(200).json(categoryData)
+  } catch(err) {
+    res.status(500).json(err);
+  }
+
 });
 
 router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategoryData = await Category.create(req.body);
+    res.status(200).json(newCategoryData)
+  } catch(err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
